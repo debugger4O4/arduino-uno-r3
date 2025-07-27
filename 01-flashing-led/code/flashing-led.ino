1233ae61f4a16963a2239daabdaca73e1c90e51d
seera <now-kirill@ya.ru>
2025-07-27 14:46:19 +0300

    Lesson 1.

--- 01-flashing-led/code/flashing-led.ino
+++ 01-flashing-led/code/flashing-led.ino
@@ -1,12 +1,12 @@
-int led = 12;
+int led = 12; // Контакт 12 подключен к светдиоду через резистор.
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT); // Инициализация цифрового вывода как выход.
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
+  digitalWrite(led, HIGH); // Включить светодиод.
+  delay(100); // Подождать 100мс.
+  digitalWrite(led, LOW); // Выключить светодиод.
+  delay(100); // Подождать 100мс.
 }
