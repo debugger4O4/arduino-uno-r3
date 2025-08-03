dc21fb6dfc31c13f8631fb1c31186fe23c60c297
seera <now-kirill@ya.ru>
2025-08-03 20:45:58 +0300

    Lesson 6.

---
+++ 06-led-control-via-shift-register/README.md
@@ -0,0 +1,3 @@
+## Управление светодиодами через регистр сдвига.
+
+![06-led-control-via-shift-register.gif](content/06-led-control-via-shift-register.gif)
---
+++ 06-led-control-via-shift-register/code/led-control-via-shift-register.ino
@@ -0,0 +1,34 @@
+// Определение номеров пинов Arduino, подключенных к микросхеме 74HC595.
+int latchPin = 8; // Пин, подключенный к ST_CP (RCLK) регистра сдвига.
+int clockPin = 12; // Пин, подключенный к SH_CP (SRCLK) регистра сдвига.
+int dataPin = 11; // Пин, подключенный к DS (DATA) регистра сдвига.
+
+void setup() {
+  // Настраиваем пины микроконтроллера как выходы.
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  // Инициализируем последовательное соединение для отладки.
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Перебираем числа от 0 до 255 для отображения на светодиодах.
+  for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
+    // Выводим текущее число в последовательный порт.
+    Serial.print(numberToDisplay);
+    Serial.print(" => ");
+    // Выводим двоичное представление числа.
+    Serial.println(numberToDisplay, BIN);
+    
+    // Опускаем latchPin перед началом передачи данных.
+    digitalWrite(latchPin, LOW);
+    // Отправляем биты в регистр сдвига.
+    shiftOut(dataPin, clockPin, LSBFIRST, numberToDisplay);
+    // Поднимаем latchPin после передачи данных для активации выходов регистра сдвига.
+    digitalWrite(latchPin, HIGH);
+    
+    // Задержка между обновлениями светодиодов.
+    delay(1000);
+  }
+}
